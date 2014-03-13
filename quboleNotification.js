773bb1d255f3d9e49dc851c575b2aefb7dc0b506
hanzenlim <hanzenlim@gmail.com>
2014-03-13 14:28:09 -0700

    loop through all keys in qbListenerMap

--- quboleNotification.js
+++ quboleNotification.js
@@ -10,23 +10,24 @@ if(typeof qbListenerMap == 'undefined')
 	$('.query_hists.qbol-table-query-hist.table').unbind("DOMSubtreeModified");
 	$('.query_hists.qbol-table-query-hist.table').bind("DOMSubtreeModified", function(){ 
 		var value = $('.query_head_status_draft.status-inline span').html(); 
-
-		console.log('change');
 		setTimeout(function() { 
 			var value = $("tr[id=" + com_id +"] > td:nth-child(2) icon").attr('title');
 
 			console.log(com_id);
-			if(value=="Success" && qbListenerMap[com_id]){ 
+			for(var key in qbListenerMap){
+			   if(value=="Success" && qbListenerMap[key]){ 
 		   		alert("Job:" + com_id + " succeeded");
 
 		   		if($.isEmptyObject(qbListenerMap)){
-		   			$('.query_hists.qbol-table-query-hist.table').unbind();
+		   			$('.query_hists.qbol-table-query-hist.table').unbind("DOMSubtreeModified");
 		   		} 
-		   		delete qbListenerMap[com_id];
-			}else if(value=="Failed" && qbListenerMap[com_id]){
+		   		delete qbListenerMap[key];
+			   }else if(value=="Failed" && qbListenerMap[key]){
 				alert("Job:" + com_id + " failed"); 
-				delete qbListenerMap[com_id];
+				delete qbListenerMap[key];
+			   }	
 			}
+			
 		}, 1000);
 
 	}); 
